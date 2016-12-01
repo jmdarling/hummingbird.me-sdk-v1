0204a58cd73691d41381bab238c45cff8e5ab449
Jonathan Darling <jmdarling@me.com>
2016-12-01 09:04:48 -0600

    Adds docs and more tests.

--- src/app.js
+++ src/app.js
@@ -4,22 +4,48 @@ let config = require('./config')
 
 module.exports = class Hummingbird {
 
+  /**
+   * Creates an instance of Hummingbird.
+   *
+   * @param {object} configOverride
+   */
   constructor (configOverride) {
     if (configOverride != null) {
-      this.config = Object.assign(config, configOverride)
+      this.config = Object.assign({}, config, configOverride)
     } else {
       this.config = config
     }
   }
 
+  /**
+   * Gets the anime for the provided ID.
+   *
+   * @param {string} id The ID for which to get the anime.
+   *
+   * @returns The anime for the provided ID.
+   */
   getAnimeById (id) {
     return this._getFromUrl(`${this.config.apiBaseUrl}/anime/${id}`)
   }
 
+  /**
+   * Returns an array of anime matching the provided search term.
+   *
+   * @param {string} termToSearch The search term to find anime for.
+   *
+   * @returns An array of anime matching the provided search term.
+   */
   searchAnime (termToSearch) {
     return this._getFromUrl(`${this.config.apiBaseUrl}/search/anime?query=${termToSearch}`)
   }
 
+  /**
+   * Performs a GET request to the provided URL.
+   *
+   * @param {string} url The URL to make the get request to.
+   *
+   * @returns A promise with the HTTP response.
+   */
   _getFromUrl (url) {
     return new Promise((resolve, reject) => {
       request.get(url, (error, response) => {
@@ -28,7 +54,11 @@ module.exports = class Hummingbird {
         }
 
         if (response.body != null) {
-          response.body = JSON.parse(response.body)
+          try {
+            response.body = JSON.parse(response.body)
+          } catch (e) {
+            return reject(e)
+          }
         }
 
         return resolve(response)
--- test/test.js
+++ test/test.js
@@ -1,11 +1,51 @@
 /* eslint-env mocha */
 const assert = require('assert')
+const config = require('../bin/config')
+const Hummingbird = require('../bin/app')
 
 describe('Hummingbird', () => {
+  describe('#constructor', () => {
+    it('Uses the default config if no override is provided', () => {
+      // Arrange.
+      // Act.
+      const hummingbirdClient = new Hummingbird()
+
+      // Assert.
+      for (let configValueKey in config) {
+        if (!config.hasOwnProperty(configValueKey)) {
+          continue
+        }
+
+        assert(hummingbirdClient.config[configValueKey] != null)
+        assert(hummingbirdClient.config[configValueKey] === config[configValueKey])
+      }
+    })
+
+    it('Uses the provided values if the optional override config is provided', () => {
+      // Arrange.
+      const configOverride = {
+        apiBaseUrl: 'https://test.com',
+        testValue: 'test'
+      }
+
+      // Act.
+      const hummingbirdClient = new Hummingbird(configOverride)
+
+      // Assert.
+      for (let configValueKey in configOverride) {
+        if (!configOverride.hasOwnProperty(configValueKey)) {
+          continue
+        }
+
+        assert(hummingbirdClient.config[configValueKey] != null)
+        assert(hummingbirdClient.config[configValueKey] === configOverride[configValueKey])
+      }
+    })
+  })
+
   describe('#getAnimeById(id)', () => {
     it('Correctly gets an anime when a valid ID is provided.', () => {
       // Arrange.
-      const Hummingbird = require('../src/app')
       const hummingbirdClient = new Hummingbird()
       const idToSearch = 1
 
@@ -25,7 +65,6 @@ describe('Hummingbird', () => {
   describe('#searchAnime(searchTerm)', () => {
     it('Correctly gets at least the expected anime, when the title of a known anime is provided.', () => {
       // Arrange.
-      const Hummingbird = require('../bin/app')
       const hummingbirdClient = new Hummingbird()
       const termToSearch = 'Cowboy Bebop'
 
